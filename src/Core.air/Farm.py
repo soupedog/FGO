5b520159c7d6be0e3181e8d9ae3fdaa623ae9000
Xavier.Wang <xavierpe@qq.com>
2022-07-10 23:05:40 +0800

    添加刷无限池脚本

---
+++ src/Core.air/Farm.py
@@ -0,0 +1,65 @@
+# sys.path.append("/Core.air")
+from airtest.cli.parser import cli_setup
+from airtest.core.api import *
+
+import Core
+from entity.Apple import Apple
+
+if not cli_setup():
+    auto_setup(__file__,
+               logdir=True,
+               devices=[
+                   "android://127.0.0.1:5037/127.0.0.1:49735?cap_method=JAVACAP&&ori_method=MINICAPORI&&touch_method=MINITOUCH",
+               ],
+               project_root="F:/Airtest/FGO/src/Core.air"
+               )
+
+
+def tu1():
+    Core.block_to_area_ready()
+
+    Core.skill(1, 1, None)
+
+    Core.skill(2, 1, None)
+    Core.skill(2, 2, 1)
+    Core.skill(2, 3, 1)
+
+    Core.skill(3, 1, None)
+    Core.skill(3, 2, 1)
+    Core.skill(3, 3, 1)
+
+    Core.start_attack()
+    Core.attack_single_ultimate_combo(1)
+
+
+def tu2():
+    Core.block_to_area_ready()
+
+    Core.start_attack()
+    Core.attack_single_ultimate_combo(1)
+
+
+def tu3():
+    Core.block_to_area_ready()
+
+    Core.start_attack()
+    Core.attack_single_ultimate_combo(1)
+
+
+current_number = 0
+max_number = 10
+while True:
+    tu1()
+
+    tu2()
+
+    tu3()
+
+    sleep(30)
+
+    Core.close_result()
+
+    current_number = Core.check_and_eat_apple(Apple.GOLDEN, current_number, max_number)
+
+    Core.select_friend()
+
---
+++ src/Core.air/entity/Apple.py
@@ -0,0 +1,8 @@
+from enum import unique, Enum
+
+
+@unique
+class Apple(Enum):
+    GOLDEN = "金苹果"
+    SILVER = "银苹果"
+    BRONZE = "铜苹果"
